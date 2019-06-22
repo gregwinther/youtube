36da333ad21117c2cfdb349e4011cd82278a6b74
gregwinther <sebastwi@uio.no>
2019-06-22 23:15:50 +0200

    Add zombie model.

---
+++ sizr.py
@@ -0,0 +1,120 @@
+"""
+The Zombie Apocaplypse!
+
+S' = sigma - beta*S*Z - delta_S*S
+I' = beta*S*Z - rho*I - delta_I*I
+Z' = rho*I - alpha*S*Z
+R' = delta_S*S + delta_I*I + alpha*S*Z
+"""
+
+import numpy as np 
+from matplotlib import pyplot as plt
+from ODESolver import ForwardEuler
+
+class SIZR:
+    def __init__(
+        self, sigma, beta, rho, delta_S, delta_I, alpha, S0, I0, Z0, R0
+    ):
+        """
+        The Zombie class
+
+        initial value:  S0, I0, Z0, R0
+        """
+
+        if isinstance(sigma, (float, int)):
+            self.sigma = lambda t: sigma
+        elif callable(sigma):
+            self.sigma = sigma
+
+        if isinstance(beta, (float, int)):
+            self.beta = lambda t: beta
+        elif callable(beta):
+            self.beta = beta
+
+        if isinstance(rho, (float, int)):
+            self.rho = lambda t: rho
+        elif callable(rho):
+            self.rho = rho
+
+        if isinstance(delta_S, (float, int)):
+            self.delta_S = lambda t: delta_S
+        elif callable(delta_S):
+            self.delta_S = delta_S
+
+        if isinstance(delta_I, (float, int)):
+            self.delta_I = lambda t: delta_I
+        elif callable(delta_I):
+            self.delta_I = delta_I
+
+        if isinstance(alpha, (float, int)):
+            self.alpha = lambda t: alpha
+        elif callable(alpha):
+            self.alpha = alpha
+
+        self.initial_conditions = [S0, I0, Z0, R0]
+
+    def __call__(self, u, t):
+        """RHS of system of ODEs"""
+
+        S, I, Z, _ = u 
+
+        return np.asarray([
+            self.sigma(t) - self.beta(t)*S*Z - self.delta_S(t)*S,
+            self.beta(t)*S*Z - self.rho(t)*I - self.delta_I(t)*I,
+            self.rho(t)*I - self.alpha(t)*S*Z,
+            self.delta_S(t)*S + self.delta_I(t)*I + self.alpha(t)*I 
+        ])
+
+if __name__ == "__main__":
+
+    """ The three phases of the Zombie apocaplypse
+
+    Phase 1: initial phase
+    Lasts four hours. Some humans meet one zombie.
+    sigma = 20, beta = 0.03, rho = 1, S0 = 60, Z0 = 1
+
+    Phase 2: Hysteria
+    Lasts 24 hours. Zombie threat is evident.
+    beta = 0.0012, alpha = 0.0016, delta_I = 0.014, sigma = 2,
+    rho = 1
+
+    Phase 3: Counter-attack
+    Lasts five hours.
+    alpha = 0.006, beta = 0 (no humans are infected),
+    delta_S = 0.007, rho = 1, delta_I = 0.05
+    """
+
+    beta = lambda t: 0.03 if t < 4 else (0.0012 if t > 4 and t < 28 else 0)
+    alpha = lambda t: 0 if t < 4 else (0.0016 if t > 4 and t < 28 else 0.05)
+    sigma = lambda t: 20 if t < 4 else (2 if t > 4 and t < 28  else 0)
+    rho = 1
+    delta_I = lambda t: 0 if t < 4 else (0.014 if t > 4 and t < 28 else 0.05)
+    delta_S = lambda t: 0 if t < 28 else 0.007
+
+    # beta = 0.012
+    # alpha = 0.0016
+    # sigma = 2
+    # rho = 1
+    # delta_I = 0.014
+    # delta_S = 0.0
+
+    S0 = 60
+    I0 = 0
+    Z0 = 1
+    R0 = 0
+
+    zombie_model = SIZR(
+        sigma, beta, rho, delta_S, delta_I, alpha, S0, I0, Z0, R0
+    )
+    solver = ForwardEuler(zombie_model)
+    solver.set_initial_conditions(zombie_model.initial_conditions)
+    
+    time_steps = np.linspace(0, 33, 1001)
+    u, t = solver.solve(time_steps)
+
+    plt.plot(t, u[:, 0], label="Susceptible humans")
+    plt.plot(t, u[:, 1], label="Infected humans")
+    plt.plot(t, u[:, 2], label="Zombies")
+    plt.plot(t, u[:, 3], label="Dead")
+    plt.legend()
+    plt.show()
